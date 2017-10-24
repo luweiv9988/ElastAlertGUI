57b34bf0e5243133cb349b96a73fc41da87cd0f5
luwei <luwei@think-land.com>
2017-10-24 15:01:01 +0800

    modifiy own elastalert path

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 SECRET_KEY = 'RIvJZJNplu0qr2iLmFAOMg31hRpGo/3fhfcXqmNYDE0'
 DEBUG = True
 ELASTALERT_PATH = '/opt/elastalert/'
-DATABASE_PATH = '/var/www/html/ElasAlertGUI/database.db'
+DATABASE_PATH = '/root/ElastAlertGUI/database.db'
 RULES_PATH = '/opt/elastalert/rules/'
 BACKUP_PATH = '/opt/elastalert/backup/'
 MAIN_CONFIG = '/opt/elastalert/config.yaml'
-DOC_TYPES = [('test','test'),('test2'.'test2')]
+DOC_TYPES = [('test','test'),('test2','test2')]
 RULE_TYPES = [('frequency','frequency')]
 ALERT_TYPES = [('email','email')]
