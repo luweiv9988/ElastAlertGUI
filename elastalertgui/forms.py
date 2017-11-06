dbb39df619dc87f1fd7eb97a4d71be3df93deb16
luwei <luwei@think-land.com>
2017-11-06 17:31:18 +0800

    暂时先去除选择框，默认使用文本接受

--- elastalertgui/forms.py
+++ elastalertgui/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, TextAreaField
 from wtforms.validators import DataRequired
-from config import DOC_TYPES, RULE_TYPES, ALERT_TYPES
+#from config import DOC_TYPES, RULE_TYPES, ALERT_TYPES
+from config import RULE_TYPES, ALERT_TYPES
 
 class LoginForm(FlaskForm):
     user_login = StringField('user_login', validators=[DataRequired("Please enter your login")])
@@ -15,18 +16,20 @@ class PassChangeForm(FlaskForm):
 class RuleForm(FlaskForm):
     name = StringField('name', validators=[DataRequired("Please enter rule name")])
     type = SelectField('type', choices=RULE_TYPES, validators=[DataRequired("Please enter rule type")])
-    index = StringField('index', default='filebeat-*', validators=[DataRequired("Please enter index name")])
+    index = StringField('index', default='logstash-supplier-log-*', validators=[DataRequired("Please enter index name")])
     num_events = StringField('num_events', default='1', validators=[DataRequired("Please enter num events")])
     timeframe = StringField('timeframe', default='1', validators=[DataRequired("Please enter time frame")])
     timeframe2 = SelectField('timeframe2', choices=[('seconds:', 'seconds'), ('minutes:', 'minutes'), ('hours:', 'hours'), ('days:', 'days'), ('weeks:', 'weeks')], validators=[DataRequired()])
-    filter = SelectField('filter', choices=DOC_TYPES, validators=[DataRequired("Please enter filter type")])
-    filter2 = StringField('filter2', default='Your Search...', validators=[DataRequired("Please enter filter query")])
+    filter = StringField('filter', default='http:404', validators=[DataRequired("Please enter Elasticsearch Condition")])
+    ## just remove one filed ,it will be add in next version
+    #filter = SelectField('filter', choices=DOC_TYPES, validators=[DataRequired("Please enter filter type")]) 
+    #filter2 = StringField('filter2', default='Your Search...', validators=[DataRequired("Please enter filter query")])
     alert = SelectField('alert', choices=ALERT_TYPES, validators=[DataRequired("Please enter alert type")])
-    email = StringField('email', default='email@domain.com', validators=[DataRequired("Please enter emails addresses")])
-    smtp_auth_file = StringField('auth',default='smtp_auth_file.yaml', validators=[DataRequired("Please enter smtp auth path")])
-    smtp_host = StringField('host',default='smtp.exmail.qq.com', validators=[DataRequired("Please enter smtp_server ip")])
-    smtp_port = StringField('port',default='25', validators=[DataRequired("Please enter smtp_server port")])
-    
+    email = StringField('email', default='luwei@think-land.com', validators=[DataRequired("Please enter emails addresses")])
+    smtp_auth_file = StringField('smtp_auth_file',default='smtp_auth_file.yaml', validators=[DataRequired("Please enter smtp auth path")])
+    smtp_host = StringField('smtp_host',default='smtp.exmail.qq.com', validators=[DataRequired("Please enter smtp_server ip")])
+    smtp_port = StringField('smtp_port',default='25', validators=[DataRequired("Please enter smtp_server port")])
+
 
     saving_button = SubmitField(label='Save rule')
     goback_button = SubmitField(label='Go Back')
